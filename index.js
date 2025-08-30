c9c5a26d6850220690c30d2f8c70bfd18ce5c25a
henriqueritter <henrique-ritter@hotmail.com>
2025-08-29 21:28:45 -0300

    feat: dynamically create track and volume controls HTML elements

--- index.js
+++ index.js
@@ -10,11 +10,53 @@ function createTrackElement({ id, name, path }) {
     { id: 1, text: 'Track 1', className: 'trackName' }
   );
 
+  const { htmlMuteButton, htmlVolumeInput } = createTrackVolumeControls({
+    parentHTMLElement: htmlTrackElement,
+  });
+
+  htmlMuteButton.addEventListener('click', () => {});
+
   htmlTracksSection.insertAdjacentElement('beforeend', htmlTrackElement);
 }
 
 function loadTracks() {
   createTrackElement({ id: 1, name: 'test', path: './test.mp3' });
+}
+
+function createTrackVolumeControls({
+  parentHTMLElement,
+  position = 'beforeend',
+}) {
+  const htmlTrackVolumeControlsElement = document.createElement('div');
+  htmlTrackVolumeControlsElement.setAttribute('class', 'volumeControls');
+
+  const htmlMuteButton = document.createElement('button');
+  htmlMuteButton.setAttribute('id', 'mute');
+  htmlMuteButton.setAttribute('class', 'muteButton');
+  htmlMuteButton.innerText = 'Mute';
+
+  htmlTrackVolumeControlsElement.insertAdjacentElement(
+    'beforeend',
+    htmlMuteButton
+  );
+
+  const htmlVolumeInput = document.createElement('input');
+  htmlVolumeInput.setAttribute('id', 'volume');
+  htmlVolumeInput.setAttribute('name', 'volume');
+  htmlVolumeInput.setAttribute('class', 'trackVolume');
+  htmlVolumeInput.setAttribute('type', 'range');
+
+  htmlTrackVolumeControlsElement.insertAdjacentElement(
+    'beforeend',
+    htmlVolumeInput
+  );
+
+  parentHTMLElement.insertAdjacentElement(
+    position,
+    htmlTrackVolumeControlsElement
+  );
+
+  return { htmlMuteButton, htmlVolumeInput };
 }
 
 function createHTMLTextElement(
