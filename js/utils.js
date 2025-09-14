bcccffc064b6aeb433e3a3f682e89c89db795d10
henriqueritter <henrique-ritter@hotmail.com>
2025-09-14 15:45:10 -0300

    feat: added media query to scale page on mobile devices

--- js/utils.js
+++ js/utils.js
@@ -1,8 +1,24 @@
 async function getSongAndTracksData() {
-  const response = await fetch(
+  /*const response = await fetch(
     'https://pub-2ee020cd36f344d7aa50a37abdbf165b.r2.dev/%0Amultitrack-getworship-umnovodia.json.json'
-  );
-  const { data } = await response.json();
+  );*/
+  //const { data } = await response.json();
+
+  const data = {
+    songInfo: {
+      name: 'Um novo dia',
+      author: 'Get Worship',
+      currentTime: 0,
+      duration: 0,
+    },
+    tracks: [
+      {
+        id: 1,
+        name: 'Música',
+        filePath: 'assets/click.mp3',
+      },
+    ],
+  };
 
   if (!data) return 'Response is empty.';
 
